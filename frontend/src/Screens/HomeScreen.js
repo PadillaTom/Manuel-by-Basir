717b4e4c22ccfdfe8e09e65de4e2479cc8cf2a94
Tomas Padilla <padillatomasagustin@gmail.com>
2020-11-26 15:28:39 +0100

    Frontend and Backend Working

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 // Utils:
 import data from '../Utils/data';
 // Components:
 import Product from '../Components/Product';
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchData();
+  }, [products]);
+
   return (
     <>
       <div className='row center'>
-        {data.products.map((product) => {
+        {products.map((product) => {
           return <Product key={product._id} product={product}></Product>;
         })}
       </div>
