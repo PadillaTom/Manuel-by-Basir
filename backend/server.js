da23b17e782fe8bfd037c28c7bde263b5f5a1c58
Tomas Padilla <padillatomasagustin@gmail.com>
2020-11-26 14:20:45 +0100

    First NODE Server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
 
+// Utils:
+import data from './data.js';
+
 const app = express();
 
+// Products
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+// Server
 app.get('/', (req, res) => {
   res.send('Server is Ready');
 });
+const port = process.env.PORT || 5000;
 app.listen(5000, () => {
-  console.log('Serve at http://localhost:5000');
+  console.log(`Serve at http://localhost:${port}`);
 });
